198853792b51eeae8674278b53913bb1c4e965ff
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-04-24 16:08:32 +0900

    날씨정보 받아오는 서버

--- weather-data-api/src/main/java/com/weather/weatherdataapi/WeatherDataApiApplication.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/WeatherDataApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherDataApiApplication {
-
+    //날씨정보를 받아오는 서버
     public static void main(String[] args) {
         SpringApplication.run(WeatherDataApiApplication.class, args);
     }
