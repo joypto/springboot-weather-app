57e91871087e40c1d0c6e05cd80a1546102a29fe
joychae <sooie35@gmail.com>
2021-04-24 15:46:53 +0900

    Initial commit

---
+++ origin-api/src/test/java/com/weather/originapi/OriginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.originapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/WeatherDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.weather.weatherdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDataApiApplication.class, args);
+    }
+
+}
