c15a77b47a996dc093bfcd78cad63e623cbac120
joychae <sooie35@gmail.com>
2021-04-27 21:37:04 +0900

    ReverseGeoCoding API Add

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/OpenApiController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/OpenApiController.java
@@ -1,4 +1,22 @@
 package com.weather.weatherdataapi.controller;
 
+import com.weather.weatherdataapi.model.dto.WeatherDataRequestDto;
+import com.weather.weatherdataapi.util.ReverseGeoCoding;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OpenApiController {
+
+    private final ReverseGeoCoding reverseGeoCoding;
+
+    @GetMapping("/api/weather/data")
+    public String getAllWeatherData(@RequestBody WeatherDataRequestDto weatherDataRequestDto) throws ParseException {
+        return reverseGeoCoding.reverseGeocoding(weatherDataRequestDto.getLongitude(), weatherDataRequestDto.getLatitude());
+    }
+
 }
--- weather-data-api/src/main/java/com/weather/weatherdataapi/util/ReverseGeoCoding.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/util/ReverseGeoCoding.java
@@ -1,5 +1,9 @@
 package com.weather.weatherdataapi.util;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,17 +13,32 @@ import javax.annotation.PostConstruct;
 @Component
 public class ReverseGeoCoding {
 
-//    @PostConstruct
-    public void reverseGeocoding() {
+    public String reverseGeocoding(String longitude, String latitude) throws ParseException {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-NCP-APIGW-API-KEY-ID", "et23d38qwi");
         headers.add("X-NCP-APIGW-API-KEY", "6RmECdY1MD4qhrXo7UNCFIzkfBr2XmheawWfrB8Y");
 
-        String locate = "128.12345,37.98776";
+        String locate = longitude + "," + latitude;
         String url = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?sourcecrs=epsg:4326&orders=legalcode,admcode&output=json&coords=" + locate;
         ResponseEntity<String> responseEntity = rest.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
-        System.out.println(responseEntity);
+
+        String data = responseEntity.getBody();
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(data);
+        JSONArray results = (JSONArray) jsonObject.get("results");
+        JSONObject region = (JSONObject) results.get(1);
+        JSONObject region2 = (JSONObject) region.get("region");
+        JSONObject area1 = (JSONObject) region2.get("area1");
+        String area1_name = (String) area1.get("name");
+        JSONObject area2 = (JSONObject) region2.get("area2");
+        String area2_name = (String) area2.get("name");
+        JSONObject area3 = (JSONObject) region2.get("area3");
+        String area3_name = (String) area3.get("name");
+
+        System.out.println(area1_name + " " + area2_name + " " + area3_name);
+        return area1_name + " " + area2_name + " " + area3_name;
     }
 
 }
