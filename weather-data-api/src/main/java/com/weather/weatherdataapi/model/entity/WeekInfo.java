cf424995a46501035c575c6195e26736cda11d08
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-04-28 23:45:03 +0900

    complete weekinfo

--- weather-data-api/src/main/java/com/weather/weatherdataapi/model/entity/WeekInfo.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/model/entity/WeekInfo.java
@@ -1,5 +1,6 @@
 package com.weather.weatherdataapi.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.weather.weatherdataapi.model.entity.region.Region;
 import lombok.Builder;
 import lombok.Getter;
