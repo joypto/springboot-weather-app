83c04a917c96b5fe7ad9b609277120ecb80a1a24
sanyoni <ksyline1997@gmail.com>
2021-04-28 11:47:56 +0900

    코로나 OpenApi로 받아온 정보를 DB에 저장하는 기능 구현

---
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/CoronaService.java
@@ -0,0 +1,45 @@
+package com.weather.weatherdataapi.service;
+
+import com.weather.weatherdataapi.model.entity.Corona;
+import com.weather.weatherdataapi.repository.CoronaRepository;
+import com.weather.weatherdataapi.util.openapi.corona.ICoronaInfo;
+import com.weather.weatherdataapi.util.openapi.corona.ICoronaItem;
+import com.weather.weatherdataapi.util.openapi.corona.gov.GovCoronaOpenApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CoronaService {
+
+    private final CoronaRepository coronaRepository;
+
+    private final GovCoronaOpenApi govCoronaOpenApi;
+
+    public List<Corona> fetchAndStoreCoronaInfoUsingOpenApi() {
+        Optional<ICoronaInfo> fetchedInfo = govCoronaOpenApi.getInfo();
+
+        if (fetchedInfo.isPresent() == false) {
+            return null;
+        }
+
+        ICoronaInfo info = fetchedInfo.get();
+
+        List<Corona> coronaList = new ArrayList<>(info.getItemList().size());
+
+        for (int i = 0; i < info.getItemList().size(); i++) {
+            ICoronaItem item = info.getItemList().get(i);
+
+            Corona corona = new Corona(item);
+            coronaRepository.save(corona);
+
+            coronaList.add(corona);
+        }
+
+        return coronaList;
+    }
+}
