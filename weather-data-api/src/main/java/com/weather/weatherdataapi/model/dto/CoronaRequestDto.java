12e18e451fe67fa991bb385af312c891a8c1e100
sanyoni <ksyline1997@gmail.com>
2021-04-28 22:38:13 +0900

    fix: CoronaRequestDto를 누락하고 커밋한 문제를 수정

---
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/model/dto/CoronaRequestDto.java
@@ -0,0 +1,18 @@
+package com.weather.weatherdataapi.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CoronaRequestDto {
+
+    private String latitude;
+
+    private String longitude;
+
+}
