7692d2a2b6d3d983822ba78429121dac55e3369f
sanyoni <ksyline1997@gmail.com>
2021-04-29 15:39:26 +0900

    CoronaService의 변경으로 임시적으로 코로나 Api 코드를 주석 처리함

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/OpenApiController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/OpenApiController.java
@@ -1,9 +1,7 @@
 package com.weather.weatherdataapi.controller;
 
-import com.weather.weatherdataapi.model.dto.CoronaRequestDto;
 import com.weather.weatherdataapi.model.dto.ReverseGeocodingResponseDto;
 import com.weather.weatherdataapi.model.dto.WeatherDataRequestDto;
-import com.weather.weatherdataapi.model.entity.Corona;
 import com.weather.weatherdataapi.model.entity.region.Region;
 import com.weather.weatherdataapi.repository.region.RegionRepository;
 import com.weather.weatherdataapi.service.CoronaService;
@@ -12,8 +10,10 @@ import com.weather.weatherdataapi.util.ReverseGeoCoding;
 import com.weather.weatherdataapi.util.openapi.livinghealthweather.LivingHealthWeatherApiCall;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.*;
-import retrofit2.http.Header;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
@@ -46,20 +46,20 @@ public class OpenApiController {
         return region;
     }
 
-    @GetMapping("/api/corona/data")
-    public Corona getCorona(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude, CoronaRequestDto requestDto) throws ParseException {
-        List<Corona> coronaList = coronaService.fetchAndStoreCoronaInfoUsingOpenApi();
-
-        ReverseGeocodingResponseDto reverseGeocodingResponseDto = reverseGeoCoding.reverseGeocoding(requestDto.getLongitude(), requestDto.getLatitude());
-        String areaAlias = reverseGeocodingResponseDto.getAlias();
-
-        for (Corona corona : coronaList) {
-            if (corona.getSido_name().equals(areaAlias)) {
-                return corona;
-            }
-        }
-
-        return null;
-    }
+//    @GetMapping("/api/corona/data")
+//    public Corona getCorona(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude, CoronaRequestDto requestDto) throws ParseException {
+//        coronaService.fetchAndStoreCoronaInfoUsingOpenApi();
+//
+//        ReverseGeocodingResponseDto reverseGeocodingResponseDto = reverseGeoCoding.reverseGeocoding(requestDto.getLongitude(), requestDto.getLatitude());
+//        String areaAlias = reverseGeocodingResponseDto.getAlias();
+//
+//        for (Corona corona : coronaList) {
+//            if (corona.getSido_name().equals(areaAlias)) {
+//                return corona;
+//            }
+//        }
+//
+//        return null;
+//    }
 
 }
