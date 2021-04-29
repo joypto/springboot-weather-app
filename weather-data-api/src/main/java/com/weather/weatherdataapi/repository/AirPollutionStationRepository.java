b9733ff47bcd3071eda5b9c27bb2d7e9538587ee
sanyoni <ksyline1997@gmail.com>
2021-04-29 22:25:39 +0900

    지역과 미세먼지 측정소를 매핑할 때 DB에 매핑 정보가 있다면 DB의 것을 사용하도록 구현

--- weather-data-api/src/main/java/com/weather/weatherdataapi/repository/AirPollutionStationRepository.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/repository/AirPollutionStationRepository.java
@@ -1,7 +1,12 @@
 package com.weather.weatherdataapi.repository;
 
 import com.weather.weatherdataapi.model.entity.AirPollutionStation;
+import com.weather.weatherdataapi.model.entity.region.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AirPollutionStationRepository extends JpaRepository<AirPollutionStation, Long> {
+
+    Optional<AirPollutionStation> findByRegion(Region region);
 }
