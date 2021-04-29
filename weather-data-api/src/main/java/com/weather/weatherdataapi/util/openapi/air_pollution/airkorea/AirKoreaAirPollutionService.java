bd561e0ef2f9ea239e2fd1143736d8f3f4804465
sanyoni <ksyline1997@gmail.com>
2021-04-29 22:00:19 +0900

    fix: 미세먼지 정보를 가져올 때 pm25 정보를 누락했던 문제 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/util/openapi/air_pollution/airkorea/AirKoreaAirPollutionService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/util/openapi/air_pollution/airkorea/AirKoreaAirPollutionService.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface AirKoreaAirPollutionService {
-    @GET("getMsrstnAcctoRltmMesureDnsty?returnType=xml&dataTerm=DAILY&pageNo=1&numOfRows=1")
+    @GET("getMsrstnAcctoRltmMesureDnsty?ver=1.0&returnType=xml&dataTerm=DAILY&pageNo=1&numOfRows=1")
     Call<AirKoreaAirPollutionResponse> getResponseByStationName(@Query("serviceKey") String serviceKey, @Query("stationName") String stationName);
 }
