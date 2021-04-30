330e18858a96658b485b278902aa5500328ba931
sanyoni <ksyline1997@gmail.com>
2021-04-30 11:42:05 +0900

    지역명으로 코로나 엔티티를 찾아올 수 있는 기능 구현

--- weather-data-api/src/main/java/com/weather/weatherdataapi/repository/CoronaRepository.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/repository/CoronaRepository.java
@@ -3,5 +3,9 @@ package com.weather.weatherdataapi.repository;
 import com.weather.weatherdataapi.model.entity.Corona;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CoronaRepository extends JpaRepository<Corona, Long> {
+
+    Optional<Corona> findByBigRegion(String bigRegion);
 }
--- weather-data-api/src/main/java/com/weather/weatherdataapi/service/CoronaService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/CoronaService.java
@@ -1,6 +1,7 @@
 package com.weather.weatherdataapi.service;
 
 import com.weather.weatherdataapi.model.entity.Corona;
+import com.weather.weatherdataapi.model.entity.Region;
 import com.weather.weatherdataapi.repository.CoronaRepository;
 import com.weather.weatherdataapi.util.openapi.corona.ICoronaInfo;
 import com.weather.weatherdataapi.util.openapi.corona.ICoronaItem;
@@ -27,5 +28,13 @@ public class CoronaService {
             Corona corona = new Corona(item);
             coronaRepository.save(corona);
         }
+    }
+
+    public Corona getInfoByRegion(Region region) {
+        return coronaRepository.findByBigRegion(region.getBigRegion()).orElseThrow(() -> new RuntimeException("해당하는 지역의 정보가 없습니다."));
+    }
+
+    public Corona getInfoByRegion(String bigRegion) {
+        return coronaRepository.findByBigRegion(bigRegion).orElseThrow(() -> new RuntimeException("해당하는 지역의 정보가 없습니다."));
     }
 }
