18930b8e5624ad7d29efb3e2d56aed37e361579a
sanyoni <ksyline1997@gmail.com>
2021-04-30 22:12:45 +0900

    ScoreService에 사용자 지정 가중치와 현재 날씨를 기반으로 점수를 계산하여 반환하는 메소드 작성

--- weather-data-api/src/main/java/com/weather/weatherdataapi/service/ScoreService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/ScoreService.java
@@ -1,50 +1,34 @@
 package com.weather.weatherdataapi.service;
 
-import com.weather.weatherdataapi.model.dto.ScoreRangeResponseDto;
 import com.weather.weatherdataapi.model.dto.ScoreRequestDto;
+import com.weather.weatherdataapi.model.dto.ScoreResultResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 public class ScoreService {
 
-    public ScoreRangeResponseDto getWeatherScore100(ScoreRequestDto scoreRequestDto) {
-        ArrayList<Integer> scoreRange = new ArrayList<>();
-        scoreRange.add(scoreRequestDto.getCoronaRange());
-        scoreRange.add(scoreRequestDto.getPm10Range());
-        scoreRange.add(scoreRequestDto.getPm25Range());
-        scoreRange.add(scoreRequestDto.getTempRange());
-        scoreRange.add(scoreRequestDto.getRainPerRange());
-        scoreRange.add(scoreRequestDto.getWeatherRange());
-        scoreRange.add(scoreRequestDto.getHumidityRange());
-        scoreRange.add(scoreRequestDto.getWindRange());
-        scoreRange.add(scoreRequestDto.getUvRange());
-        scoreRange.add(scoreRequestDto.getPollenRiskRange());
-        scoreRange.add(scoreRequestDto.getColdRange());
-        scoreRange.add(scoreRequestDto.getAsthmaRange());
-        scoreRange.add(scoreRequestDto.getFoodPoisonRange());
+    public int getCalculatedScore(ScoreRequestDto scoreRequestDto, ScoreResultResponseDto scoreResultResponseDto) {
+        double temporaryValue = 0;
 
-        int sum = scoreRange.stream().mapToInt(Integer::intValue).sum();
+        temporaryValue += Double.parseDouble(scoreResultResponseDto.getTempResult().get(0)) * scoreRequestDto.getTempRange();
+        temporaryValue += Double.parseDouble(scoreResultResponseDto.getRainPerResult().get(0)) * scoreRequestDto.getRainPerRange();
+        temporaryValue += Double.parseDouble(scoreResultResponseDto.getWeatherResult().get(0)) * scoreRequestDto.getWeatherRange();
+        temporaryValue += Double.parseDouble(scoreResultResponseDto.getHumidityResult().get(0)) * scoreRequestDto.getHumidityRange();
+        temporaryValue += Double.parseDouble(scoreResultResponseDto.getWindResult().get(0)) * scoreRequestDto.getWindRange();
+        temporaryValue += scoreResultResponseDto.getPm10Result() * scoreRequestDto.getPm10Range();
+        temporaryValue += scoreResultResponseDto.getPm25Result() * scoreRequestDto.getPm25Range();
+        temporaryValue += scoreResultResponseDto.getCoronaResult() * scoreRequestDto.getCoronaRange();
+        temporaryValue += scoreResultResponseDto.getUvResult() * scoreRequestDto.getUvRange();
+        temporaryValue += scoreResultResponseDto.getPollenRiskResult() * scoreRequestDto.getPollenRiskRange();
+        temporaryValue += scoreResultResponseDto.getColdResult() * scoreRequestDto.getColdRange();
+        temporaryValue += scoreResultResponseDto.getAsthmaResult() * scoreRequestDto.getAsthmaRange();
+        temporaryValue += scoreResultResponseDto.getFoodPoisonResult() * scoreRequestDto.getFoodPoisonRange();
 
-        ScoreRangeResponseDto scoreRangeResponseDto = new ScoreRangeResponseDto();
-        scoreRangeResponseDto.setCoronaRangeConvert(scoreRequestDto.getCoronaRange()*100/sum);
-        scoreRangeResponseDto.setPm10RangeConvert(scoreRequestDto.getPm10Range()*100/sum);
-        scoreRangeResponseDto.setPm25RangeConvert(scoreRequestDto.getPm25Range()*100/sum);
-        scoreRangeResponseDto.setTempRangeConvert(scoreRequestDto.getTempRange()*100/sum);
-        scoreRangeResponseDto.setRainPerRangeConvert(scoreRequestDto.getRainPerRange()*100/sum);
-        scoreRangeResponseDto.setWeatherRangeConvert(scoreRequestDto.getWeatherRange()*100/sum);
-        scoreRangeResponseDto.setHumidityRangeConvert(scoreRequestDto.getHumidityRange()*100/sum);
-        scoreRangeResponseDto.setWindRangeConvert(scoreRequestDto.getWindRange()*100/sum);
-        scoreRangeResponseDto.setUvRangeConvert(scoreRequestDto.getUvRange()*100/sum);
-        scoreRangeResponseDto.setPollenRiskRangeConvert(scoreRequestDto.getPollenRiskRange()*100/sum);
-        scoreRangeResponseDto.setColdRangeConvert(scoreRequestDto.getColdRange()*100/sum);
-        scoreRangeResponseDto.setAsthmaRangeConvert(scoreRequestDto.getAsthmaRange()*100/sum);
-        scoreRangeResponseDto.setFoodPoisonRangeConvert(scoreRequestDto.getFoodPoisonRange()*100/sum);
+        int totalScore = (int) Math.round(temporaryValue / scoreRequestDto.getSumOfAllRange());
 
-        return scoreRangeResponseDto;
+        return totalScore;
     }
 
 }
