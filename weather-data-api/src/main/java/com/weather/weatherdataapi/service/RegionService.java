49130ed37fa73f9a236190d2b06d2b3fd4b1a7bb
sanyoni <ksyline1997@gmail.com>
2021-05-01 21:01:13 +0900

    Region 정보가 DB에 있지 않을 때만 초기화하는 메소드 작성

--- weather-data-api/src/main/java/com/weather/weatherdataapi/service/RegionService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/RegionService.java
@@ -12,7 +12,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -23,64 +22,70 @@ public class RegionService {
     private final BigRegionRepository bigRegionRepository;
     private final SmallRegionRepository smallRegionRepository;
 
-    @Transactional
-    public void initializeRegionTable() {
+    public void initialize() {
         try {
-            log.info("initializeRegionTable::DB의 지역 정보 테이블을 초기화합니다.");
 
-            bigRegionRepository.deleteAll();
-            smallRegionRepository.deleteAll();
+            log.info("initialize::resources/data/region.csv 파일을 읽어 지역정보를 초기화합니다.");
 
             ClassPathResource regionCsvResource = new ClassPathResource("data/region.csv");
-
             List<RegionCsvVO> regionList = CsvParserUtil.parseCsvToObject(RegionCsvVO.class, regionCsvResource.getFile(), RegionCsvVO.getSchema());
 
-            BigRegion latestBigRegion = null;
-
-            for (RegionCsvVO regionVO : regionList) {
-                // BigRegion 에 대한 정보입니다.
-                // BigRegion 정보에 대한 행은 smallRegion 필드의 값이 비어있는 행입니다.
-                if (regionVO.isBigRegionInfo() == true) {
-
-                    // 이미 존재한다면 추가하지 않습니다.
-                    if (bigRegionRepository.findByAdmCode(regionVO.getAdmCode()) != null) {
-                        continue;
-                    }
-
-                    BigRegion newBigRegion = new BigRegion(regionVO.getBigRegion(), regionVO.getAdmCode(), regionVO.getLongitude(), regionVO.getLatitude());
-                    bigRegionRepository.save(newBigRegion);
-
-                    latestBigRegion = newBigRegion;
-                }
-                // 그 외에는 전부 소분류 지역에 대한 정보입니다.
-                else {
-
-                    // 이미 존재한다면 추가하지 않습니다.
-                    if (smallRegionRepository.findByAdmCode(regionVO.getAdmCode()) != null) {
-                        continue;
-                    }
-
-                    // 가장 최근에 저장되었거나 접근된 bigRegion이 현재 저장하려는 smallRegion을 포함하는 행정 구역이라면
-                    // DB에 접근하지 않고 캐시된 BigRegion객체를 사용합니다.
-                    BigRegion bigRegion = regionVO.getBigRegion().equals(latestBigRegion.getBigRegionName()) ? latestBigRegion : bigRegionRepository.findByBigRegionName(latestBigRegion.getBigRegionName());
-                    latestBigRegion = bigRegion;
-
-                    SmallRegion newSmallRegion = new SmallRegion(regionVO.getSmallRegion(), regionVO.getAdmCode(), regionVO.getLongitude(), regionVO.getLatitude(), bigRegion);
-                    smallRegionRepository.save(newSmallRegion);
-                }
+            if (checkRegionTableInitialized(regionList) == true) {
+                log.info("initialize::모든 지역정보가 이미 DB에 존재합니다.");
+            } else {
+                initializeRegionTable(regionList);
+                log.info("initialize::초기화를 성공적으로 마쳤습니다.");
             }
 
-            log.info("initializeRegionTable::성공적으로 초기화 하였습니다.");
         } catch (Exception e) {
             log.error(e.getMessage());
             e.printStackTrace();
-            log.error("initializeRegionTable::초기화하는 도중 문제가 발생하였습니다.");
+            log.error("initialize::초기화하는 도중 문제가 발생하였습니다.");
         }
     }
 
-    public boolean checkRegionTableInitialized() throws IOException {
-        ClassPathResource regionCsvResource = new ClassPathResource("data/region.csv");
-        List<RegionCsvVO> regionList = CsvParserUtil.parseCsvToObject(RegionCsvVO.class, regionCsvResource.getFile(), RegionCsvVO.getSchema());
+    @Transactional
+    public void initializeRegionTable(List<RegionCsvVO> regionList) {
+        bigRegionRepository.deleteAll();
+        smallRegionRepository.deleteAll();
+
+        BigRegion latestBigRegion = null;
+
+        for (RegionCsvVO regionVO : regionList) {
+            // BigRegion 에 대한 정보입니다.
+            // BigRegion 정보에 대한 행은 smallRegion 필드의 값이 비어있는 행입니다.
+            if (regionVO.isBigRegionInfo() == true) {
+
+                // 이미 존재한다면 추가하지 않습니다.
+                if (bigRegionRepository.findByAdmCode(regionVO.getAdmCode()) != null) {
+                    continue;
+                }
+
+                BigRegion newBigRegion = new BigRegion(regionVO.getBigRegion(), regionVO.getAdmCode(), regionVO.getLongitude(), regionVO.getLatitude());
+                bigRegionRepository.save(newBigRegion);
+
+                latestBigRegion = newBigRegion;
+            }
+            // 그 외에는 전부 소분류 지역에 대한 정보입니다.
+            else {
+
+                // 이미 존재한다면 추가하지 않습니다.
+                if (smallRegionRepository.findByAdmCode(regionVO.getAdmCode()) != null) {
+                    continue;
+                }
+
+                // 가장 최근에 저장되었거나 접근된 bigRegion이 현재 저장하려는 smallRegion을 포함하는 행정 구역이라면
+                // DB에 접근하지 않고 캐시된 BigRegion객체를 사용합니다.
+                BigRegion bigRegion = regionVO.getBigRegion().equals(latestBigRegion.getBigRegionName()) ? latestBigRegion : bigRegionRepository.findByBigRegionName(latestBigRegion.getBigRegionName());
+                latestBigRegion = bigRegion;
+
+                SmallRegion newSmallRegion = new SmallRegion(regionVO.getSmallRegion(), regionVO.getAdmCode(), regionVO.getLongitude(), regionVO.getLatitude(), bigRegion);
+                smallRegionRepository.save(newSmallRegion);
+            }
+        }
+    }
+
+    private boolean checkRegionTableInitialized(List<RegionCsvVO> regionList) {
 
         // 레포지토리가 비어있다면 초기화가 되어있지 않다고 간주합니다.
         if (bigRegionRepository.count() == 0 && smallRegionRepository.count() == 0) {
