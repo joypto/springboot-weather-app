e074aa5124ff436cd57c6f0e0d5a83cc93eca42a
sanyoni <ksyline1997@gmail.com>
2021-05-01 18:46:29 +0900

    BigRegion과 SmallRegion 레포지토리 클래스 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/repository/BigRegionRepository.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/repository/BigRegionRepository.java
@@ -4,4 +4,8 @@ import com.weather.weatherdataapi.model.entity.BigRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BigRegionRepository extends JpaRepository<BigRegion, Long> {
+
+    BigRegion findByAdmCode(String admCode);
+
+    BigRegion findByBigRegionName(String bigRegionName);
 }
--- weather-data-api/src/main/java/com/weather/weatherdataapi/repository/SmallRegionRepository.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/repository/SmallRegionRepository.java
@@ -4,4 +4,7 @@ import com.weather.weatherdataapi.model.entity.SmallRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SmallRegionRepository extends JpaRepository<SmallRegion, Long> {
+
+    SmallRegion findByAdmCode(String admCode);
+
 }
