9390de041dcb92126f3d03f910a7f01c58626d56
sanyoni <ksyline1997@gmail.com>
2021-05-01 21:01:21 +0900

    서버를 시작할 때, region.csv 파일 내에 포함된 지역정보가 없다면 초기화하도록 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/WeatherDataApplicationRunner.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/WeatherDataApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.weather.weatherdataapi;
 
 import com.weather.weatherdataapi.service.CoronaService;
+import com.weather.weatherdataapi.service.RegionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -14,12 +15,15 @@ public class WeatherDataApplicationRunner implements ApplicationRunner {
 
     private final CoronaService coronaService;
 
+    private final RegionService regionService;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        log.info("run::서버를 시작합니다. OpenAPI를 사용하여 DB의 데이터를 갱신합니다.");
+        log.info("run::OpenAPI를 사용하여 DB의 데이터를 갱신합니다.");
         coronaService.fetchAndStoreCoronaInfoUsingOpenApi();
-
         log.info("run::성공적으로 갱신했습니다.");
+
+        regionService.initialize();
     }
 
 }
