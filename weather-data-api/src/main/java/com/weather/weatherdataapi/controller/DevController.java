c7021061a5ddbc387ee277560d268a0a15f91e2b
joychae <sooie35@gmail.com>
2021-05-04 19:42:54 +0900

    fix: DevController 생성 및 개발용 api 해당 클래스로 이동

---
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/DevController.java
@@ -0,0 +1,55 @@
+package com.weather.weatherdataapi.controller;
+
+import com.weather.weatherdataapi.model.dto.CoordinateDto;
+import com.weather.weatherdataapi.model.dto.responsedto.ReverseGeocodingResponseDto;
+import com.weather.weatherdataapi.model.entity.BigRegion;
+import com.weather.weatherdataapi.model.entity.SmallRegion;
+import com.weather.weatherdataapi.model.entity.info.AirPollutionInfo;
+import com.weather.weatherdataapi.repository.BigRegionRepository;
+import com.weather.weatherdataapi.repository.SmallRegionRepository;
+import com.weather.weatherdataapi.service.AirPollutionService;
+import com.weather.weatherdataapi.util.openapi.geo.kakao.KakaoGeoApi;
+import com.weather.weatherdataapi.util.openapi.geo.kakao.transcoord.KakaoGeoTranscoordResponseDocument;
+import com.weather.weatherdataapi.util.openapi.geo.naver.ReverseGeoCodingApi;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class DevController {
+
+    private final ReverseGeoCodingApi reverseGeoCodingApi;
+    private final BigRegionRepository bigRegionRepository;
+    private final SmallRegionRepository smallRegionRepository;
+    private final AirPollutionService airPollutionService;
+    private final KakaoGeoApi kakaoGeoApi;
+
+    @GetMapping("/api/air_pollution/data")
+    public AirPollutionInfo getAirPollution(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude) throws ParseException {
+        CoordinateDto coordinateDto = new CoordinateDto(longitude, latitude);
+        ReverseGeocodingResponseDto reverseGeocodingResponseDto = reverseGeoCodingApi.reverseGeocoding(coordinateDto);
+
+        BigRegion bigRegion = bigRegionRepository.findByBigRegionName(reverseGeocodingResponseDto.getBigRegion());
+        SmallRegion smallRegion = smallRegionRepository.findByBigRegionAndSmallRegionName(bigRegion, reverseGeocodingResponseDto.getSmallRegion());
+
+        AirPollutionInfo airPollution = airPollutionService.fetchAndStoreAirPollutionInfoUsingOpenApi(smallRegion);
+
+        return airPollution;
+    }
+
+    @GetMapping("/api/transcoord")
+    public String getTranscoord(@RequestParam String x, @RequestParam String y) {
+        try {
+            KakaoGeoTranscoordResponseDocument document = kakaoGeoApi.convertWGS84ToWTM(x, y);
+            return document.getX() + "," + document.getY();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+}
--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/WeatherDataController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/WeatherDataController.java
@@ -25,12 +25,6 @@ import java.io.IOException;
 @RestController
 public class WeatherDataController {
 
-    private final ReverseGeoCodingApi reverseGeoCoding;
-    private final CoronaService coronaService;
-    private final AirPollutionService airPollutionService;
-    private final KakaoGeoApi kakaoGeoOpenApi;
-    private final BigRegionRepository bigRegionRepository;
-    private final SmallRegionRepository smallRegionRepository;
     private final TotalDataService totalDataService;
 
     @GetMapping("/api/weather/data")
@@ -39,29 +33,5 @@ public class WeatherDataController {
 
         CoordinateDto coordinateDto = new CoordinateDto(longitude, latitude);
         return totalDataService.getTotalData(coordinateDto, token);
-    }
-
-    @GetMapping("/api/air_pollution/data")
-    public AirPollutionInfo getAirPollution(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude) throws ParseException {
-        CoordinateDto coordinateDto = new CoordinateDto(longitude, latitude);
-        ReverseGeocodingResponseDto reverseGeocodingResponseDto = reverseGeoCoding.reverseGeocoding(coordinateDto);
-
-        BigRegion bigRegion = bigRegionRepository.findByBigRegionName(reverseGeocodingResponseDto.getBigRegion());
-        SmallRegion smallRegion = smallRegionRepository.findByBigRegionAndSmallRegionName(bigRegion, reverseGeocodingResponseDto.getSmallRegion());
-
-        AirPollutionInfo airPollution = airPollutionService.fetchAndStoreAirPollutionInfoUsingOpenApi(smallRegion);
-
-        return airPollution;
-    }
-
-    @GetMapping("/api/transcoord")
-    public String getTranscoord(@RequestParam String x, @RequestParam String y) {
-        try {
-            KakaoGeoTranscoordResponseDocument document = kakaoGeoOpenApi.convertWGS84ToWTM(x, y);
-            return document.getX() + "," + document.getY();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "error";
-        }
     }
 }
