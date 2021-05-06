34c43069b73ddb9391d3260d2842f1ed85762f7f
joychae <sooie35@gmail.com>
2021-05-06 15:54:11 +0900

    add: bigRegionName 값으로 그에 해당하는 smallRegionList 반환해주는 api 추가

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/RegionController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/RegionController.java
@@ -1,7 +1,28 @@
 package com.weather.weatherdataapi.controller;
 
+import com.weather.weatherdataapi.model.entity.BigRegion;
+import com.weather.weatherdataapi.model.entity.SmallRegion;
+import com.weather.weatherdataapi.repository.BigRegionRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@RestController
 public class RegionController {
 
+    private final BigRegionRepository bigRegionRepository;
 
+    @GetMapping("/api/region/smallregions")
+    public List<SmallRegion> getSmallRegionNamesByBigRegionName(@RequestParam("currentBigRegionName") String bigRegionName) {
+        BigRegion bigRegion = bigRegionRepository.findByBigRegionName(bigRegionName);
+        return bigRegion.getSmallRegionList();
+    }
 
 }
