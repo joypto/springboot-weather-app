64f880964a5f3e0694d2336d5d1f52fcace4c242
sanyoni <ksyline1997@gmail.com>
2021-05-07 15:20:16 +0900

    Redis DB를 초기화하는 행위를 원자성을 가지게 했고, 초기화 도중 문제가 생길 때 로그를 남기도록 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/service/RedisService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/RedisService.java
@@ -2,8 +2,12 @@ package com.weather.weatherdataapi.service;
 
 import com.weather.weatherdataapi.repository.redis.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class RedisService {
@@ -17,15 +21,26 @@ public class RedisService {
     private final WeatherDayRedisRepository weatherDayRedisRepository;
     private final WeatherWeekRedisRepository weatherWeekRedisRepository;
 
+    @Transactional
     public void initialize() {
-        bigRegionRedisRepository.deleteAll();
-        smallRegionRedisRepository.deleteAll();
+        try {
+            bigRegionRedisRepository.deleteAll();
+            smallRegionRedisRepository.deleteAll();
 
-        airPollutionRedisRepository.deleteAll();
-        coronaRedisRepository.deleteAll();
-        livingHealthRedisRepository.deleteAll();
-        weatherDayRedisRepository.deleteAll();
-        weatherWeekRedisRepository.deleteAll();
+            airPollutionRedisRepository.deleteAll();
+            coronaRedisRepository.deleteAll();
+            livingHealthRedisRepository.deleteAll();
+            weatherDayRedisRepository.deleteAll();
+            weatherWeekRedisRepository.deleteAll();
+
+            log.info("initialize::Redis DB를 성공적으로 초기화하였습니다.");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+            log.error("initialize::Redis DB를 초기화하는 중 문제가 발생하였습니다.");
+            throw e;
+        }
+
     }
 
 }
