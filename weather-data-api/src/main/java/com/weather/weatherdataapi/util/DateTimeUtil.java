5faeb05756661015b204ac52d5a61b807497662e
sanyoni <ksyline1997@gmail.com>
2021-05-11 01:33:52 +0900

    TimeZone 관련 유틸 클래스 작성

--- weather-data-api/src/main/java/com/weather/weatherdataapi/util/DateTimeUtil.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/util/DateTimeUtil.java
@@ -2,9 +2,17 @@ package com.weather.weatherdataapi.util;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 public class DateTimeUtil {
+
+    public final static String ZONE_NAME_ASIA_SEOUL = "Asia/Seoul";
+    public final static ZoneId ZONE_ID_ASIA_SEOUL = ZoneId.of(ZONE_NAME_ASIA_SEOUL);
+    public final static TimeZone TIMEZONE_ASIA_SEOUL = TimeZone.getTimeZone(ZONE_ID_ASIA_SEOUL);
+
     public static String getDateTimeString() {
         LocalDateTime dateTime = LocalDateTime.now();
         return dateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHH"));
@@ -14,4 +22,13 @@ public class DateTimeUtil {
         LocalDate date = LocalDate.now();
         return date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
     }
+
+    public static ZonedDateTime getSeoulNow() {
+        return ZonedDateTime.now(ZONE_ID_ASIA_SEOUL);
+    }
+
+    public static ZonedDateTime convertSeoulZonedDateTime(LocalDateTime localDateTime) {
+        return ZonedDateTime.of(localDateTime, ZONE_ID_ASIA_SEOUL);
+    }
+
 }
