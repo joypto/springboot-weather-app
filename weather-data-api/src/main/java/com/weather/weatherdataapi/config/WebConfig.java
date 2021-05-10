07e3dc1d251d84deb73613b7451f462991a5cde3
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-05-10 20:11:57 +0900

    cors config 설정

---
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.weather.weatherdataapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+//                .allowedOrigins("http://**")
+//                .allowedOrigins("https://**")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name());
+    }
+}
