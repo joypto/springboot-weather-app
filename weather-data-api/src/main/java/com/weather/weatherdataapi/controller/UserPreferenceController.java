765e178d00b9795e0249c0c7835faa8440742ef9
sanyoni <ksyline1997@gmail.com>
2021-05-11 20:21:33 +0900

    /api/user/preferences 요청을 하나로 묶음

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/UserPreferenceController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/UserPreferenceController.java
@@ -1,13 +1,19 @@
 package com.weather.weatherdataapi.controller;
 
-import com.weather.weatherdataapi.exception.repository.InvalidUserException;
 import com.weather.weatherdataapi.model.dto.ScoreWeightDto;
 import com.weather.weatherdataapi.model.entity.User;
 import com.weather.weatherdataapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Slf4j
@@ -17,38 +23,28 @@ public class UserPreferenceController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/api/user/preferences")
-    public User getDefaultUserPreference(@RequestHeader("token") String token) {
-        log.info("token= '{}'", token);
+    @PostMapping("/api/user/preferences")
+    public User saveUserPreference(@RequestBody ScoreWeightDto scoreWeightDto, @RequestHeader(value = "token", required = false) String token) {
+        log.info("token='{}' \t scoreWeight={}", token, scoreWeightDto.toString());
 
+        // 이미 identification을 발급받은 클라이언트의 요청입니다.
         if (StringUtils.hasText(token)) {
-            try {
-                return userRepository.findByIdentification(token).orElseThrow(() -> new InvalidUserException());
-            } catch (InvalidUserException e) {
-                return new User("default");
-            }
+            Optional<User> queriedUser = userRepository.findByIdentification(token);
+
+            // DB에 저장되어 있는 user를 가리키는 identification라면 수정을 시도합니다.
+            if (queriedUser.isPresent())
+                return queriedUser.get();
         }
 
-        return new User("default");
-    }
+        // 이전에 identification을 발급받지 않은 사용자이거나,
+        // DB에 저장되어 있는 User의 identification이 아니라면
+        // 새로운 User를 만들어 반환합니다.
+        String newIdentification = "wl" + ZonedDateTime.now().toString() + UUID.randomUUID();
 
-    @PostMapping("/api/user/preferences")
-    public User saveUserPreference(@RequestBody ScoreWeightDto scoreWeightDto, @RequestHeader("token") String token) {
-        log.info("token='{}' \t scoreWeight={}", token, scoreWeightDto.toString());
+        User newUser = new User(newIdentification, scoreWeightDto);
+        userRepository.save(newUser);
 
-        User userPreference = new User(token, scoreWeightDto);
-        userRepository.save(userPreference);
-        return userPreference;
-    }
-
-    @PutMapping("/api/user/preferences")
-    public User updateUserPreference(@RequestBody ScoreWeightDto scoreWeightDto, @RequestHeader("token") String token) {
-        log.info("token='{}' \t scoreWeight={}", token, scoreWeightDto.toString());
-
-        User userPreference = userRepository.findByIdentification(token).orElseThrow(() -> new InvalidUserException());
-        userPreference.updateUserPreference(scoreWeightDto);
-        userRepository.save(userPreference);
-        return userPreference;
+        return newUser;
     }
 
 }
