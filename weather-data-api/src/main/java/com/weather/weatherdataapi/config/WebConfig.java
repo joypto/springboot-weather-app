c94c7a9576d2f9551a022b28601f16b1e7a57cc5
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-05-11 16:45:29 +0900

    GET,HEAD,POST,PUT,DELETE Cors 해결 method 추가

--- weather-data-api/src/main/java/com/weather/weatherdataapi/config/WebConfig.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.weather.weatherdataapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*").allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name());
     }
 }
