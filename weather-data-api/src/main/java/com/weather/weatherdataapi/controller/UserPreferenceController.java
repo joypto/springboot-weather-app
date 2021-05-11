15aedfeb7e98752111cafb4424c6a3a22667407f
sanyoni <ksyline1997@gmail.com>
2021-05-11 17:27:58 +0900

    요청이 올 때 로그를 남기는 코드 추가

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/UserPreferenceController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/UserPreferenceController.java
@@ -5,10 +5,12 @@ import com.weather.weatherdataapi.model.dto.ScoreWeightDto;
 import com.weather.weatherdataapi.model.entity.User;
 import com.weather.weatherdataapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class UserPreferenceController {
@@ -17,19 +19,23 @@ public class UserPreferenceController {
 
     @GetMapping("/api/user/preferences")
     public User getDefaultUserPreference(@RequestHeader("token") String token) {
+        log.info("token= '{}'", token);
+
         if (StringUtils.hasText(token)) {
-            System.out.println("token 값: " + token);
             try {
                 return userRepository.findByIdentification(token).orElseThrow(() -> new InvalidUserException());
             } catch (InvalidUserException e) {
                 return new User("default");
             }
         }
+
         return new User("default");
     }
 
     @PostMapping("/api/user/preferences")
     public User saveUserPreference(@RequestBody ScoreWeightDto scoreWeightDto, @RequestHeader("token") String token) {
+        log.info("token='{}' \t scoreWeight={}", token, scoreWeightDto.toString());
+
         User userPreference = new User(token, scoreWeightDto);
         userRepository.save(userPreference);
         return userPreference;
@@ -37,6 +43,8 @@ public class UserPreferenceController {
 
     @PutMapping("/api/user/preferences")
     public User updateUserPreference(@RequestBody ScoreWeightDto scoreWeightDto, @RequestHeader("token") String token) {
+        log.info("token='{}' \t scoreWeight={}", token, scoreWeightDto.toString());
+
         User userPreference = userRepository.findByIdentification(token).orElseThrow(() -> new InvalidUserException());
         userPreference.updateUserPreference(scoreWeightDto);
         userRepository.save(userPreference);
