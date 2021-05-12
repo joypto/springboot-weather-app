f0a6069560677451fde7973446f7e84e54c2232f
joychae <sooie35@gmail.com>
2021-05-12 21:29:30 +0900

    fix: 모든 api 요청 헤더에 Identification 헤더 돌아가도록 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/RegionController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/RegionController.java
@@ -1,15 +1,12 @@
 package com.weather.weatherdataapi.controller;
 
+import com.weather.weatherdataapi.Global;
 import com.weather.weatherdataapi.model.entity.BigRegion;
-import com.weather.weatherdataapi.model.entity.SmallRegion;
 import com.weather.weatherdataapi.repository.BigRegionRepository;
-import com.weather.weatherdataapi.service.RegionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +18,12 @@ public class RegionController {
     private final BigRegionRepository bigRegionRepository;
 
     @GetMapping("/api/regions")
-    public List<BigRegion> getAllRegionName() {
-        return bigRegionRepository.findAll();
+    public ResponseEntity<List<BigRegion>> getAllRegionName(@RequestHeader(value = Global.IDENTIFICATION_TEXT, required = false) String identification) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(Global.IDENTIFICATION_TEXT, identification);
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(bigRegionRepository.findAll());
     }
 
 }
