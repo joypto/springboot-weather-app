0887b318d49806d7c5e587fb5024604dc64e5b71
joychae <sooie35@gmail.com>
2021-05-12 15:04:15 +0900

    add: TotalDataController에서 Header로 identification 값 내려주도록 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/model/dto/RegionDto.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/model/dto/RegionDto.java
@@ -1,10 +1,13 @@
 package com.weather.weatherdataapi.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RegionDto {
 
     /**
