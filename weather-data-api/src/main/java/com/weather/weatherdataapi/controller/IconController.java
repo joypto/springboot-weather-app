c549837945d479e95a75aefbcb21819f23758561
joychae <sooie35@gmail.com>
2021-05-14 23:09:52 +0900

    fix: message controller 헤더 세팅

--- weather-data-api/src/main/java/com/weather/weatherdataapi/controller/IconController.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/controller/IconController.java
@@ -1,14 +1,14 @@
 package com.weather.weatherdataapi.controller;
 
+import com.weather.weatherdataapi.Global;
 import com.weather.weatherdataapi.service.IconService;
-import com.weather.weatherdataapi.util.IconUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +17,13 @@ public class IconController {
     private final IconService iconService;
 
     @GetMapping("/api/icon/messages")
-    public String getRandomIconMessage(@RequestParam(value = "icon") String icon) {
-        return iconService.getRandomIconMessage(icon);
+    public ResponseEntity<String> getRandomIconMessage(@RequestParam(value = "icon") String icon, @RequestHeader(value = Global.IDENTIFICATION_TEXT, required = false) String identification) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(Global.IDENTIFICATION_TEXT, identification);
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(iconService.getRandomIconMessage(icon));
     }
 
 }
