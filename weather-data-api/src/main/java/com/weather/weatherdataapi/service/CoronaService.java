e4f8f00c0774c5fb5ffe35a9ebe60584f8036623
sanyoni <ksyline1997@gmail.com>
2021-05-15 11:55:54 +0900

    fix: 최신 코로나 정보가 이미 다른 머신에 의해 DB에 저장되었을 때, 캐시를 비우지 못하여 계속 이전 데이터를 불러오는 문제 수정

--- weather-data-api/src/main/java/com/weather/weatherdataapi/service/CoronaService.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/service/CoronaService.java
@@ -91,6 +91,7 @@ public class CoronaService {
             log.error("원격 서버에서 코로나 정보를 가져오는 데 실패하였습니다.");
         }
 
+        refreshCache();
     }
 
     @Transactional
@@ -113,8 +114,6 @@ public class CoronaService {
             CoronaInfo corona = new CoronaInfo(item, regionService);
             coronaRepository.save(corona);
         }
-
-        refreshCache();
 
         log.info("코로나 데이터를 성공적으로 갱신하였습니다.");
     }
