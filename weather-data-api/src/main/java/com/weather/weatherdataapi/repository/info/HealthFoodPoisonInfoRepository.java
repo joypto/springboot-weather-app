85f96f05c1b29d4c06da6c19156ddc1be9abb9b0
sanyoni <ksyline1997@gmail.com>
2021-05-28 00:01:21 +0900

    각 생활/보건기상지수 엔티티의 repository 클래스의 메소드 생성

--- weather-data-api/src/main/java/com/weather/weatherdataapi/repository/info/HealthFoodPoisonInfoRepository.java
+++ weather-data-api/src/main/java/com/weather/weatherdataapi/repository/info/HealthFoodPoisonInfoRepository.java
@@ -1,7 +1,13 @@
 package com.weather.weatherdataapi.repository.info;
 
+import com.weather.weatherdataapi.model.entity.BigRegion;
 import com.weather.weatherdataapi.model.entity.info.HealthFoodPoisonInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HealthFoodPoisonInfoRepository extends JpaRepository<HealthFoodPoisonInfo, Long> {
+
+    Optional<HealthFoodPoisonInfo> findFirstByBigRegionOrderByDateDesc(BigRegion bigRegion);
+
 }
